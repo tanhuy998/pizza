3c81845d7b43a62511495d6c621ff29352a67036
Huy Tran <tanhuy998@gmail.com>
2020-09-27 12:52:46 +0700

    add(Category)

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -8,11 +8,11 @@ use App\Category as Category;
 class CategoryController extends Controller
 {
     //
-    public function Craete(Request $_request) {
+    public function Create(Request $_request) {
 
         $new_cate = new Category();
 
-        $new_cate->Name = $_request->name;
+        $new_cate->Name = $_request->input('name');
 
         $new_cate->save();
 
